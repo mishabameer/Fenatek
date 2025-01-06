9ee1f008baf78868eff86a1ccb5ced4de53edd03
Mishab Ameer <mishab@dexlock.com>
2025-01-06 14:10:48 +0300

    Added sub category of indstrial tech

--- src/utils.js
+++ src/utils.js
@@ -92,7 +92,6 @@ export const getProductInCategory = (catName) => {
           { id: 303, name: "Dumpers and Carriers" },
           { id: 304, name: "Spider Lifters" },
           { id: 305, name: "Vehicle Aerial Platforms" },
-          // { id: 306, name: "Sandwich Panel Lifter" },
           { id: 307, name: "Cable Pulling System" },
         ],
       };
@@ -102,18 +101,65 @@ export const getProductInCategory = (catName) => {
         id: 4,
         name: "Industrial Technologies",
         products: [
-          { id: 401, name: "Packaging" },
-          { id: 402, name: "Cleaning" },
-          { id: 403, name: "Stretch Wrapping Solutions" },
-          { id: 404, name: "Industrial Vacuum Cleaner" },
-          { id: 406, name: "Safety Solutions" },
+          {
+            id: 401,
+            name: "Packaging",
+            subCategories: [
+              { id: 4011, name: "Shrink Wrap" },
+              { id: 4012, name: "Strapping Machine" },
+            ],
+          },
+          {
+            id: 402,
+            name: "Cleaning",
+            subCategories: [
+              { id: 4021, name: "Sweeping Machine" },
+              { id: 4022, name: "Pressure Cleaning" },
+              { id: 4023, name: "Steam Cleaning" },
+              { id: 4024, name: "Solar Panel Cleaning" },
+            ],
+          },
+          {
+            id: 403,
+            name: "Stretch Wrapping Solutions",
+            subCategories: [
+              { id: 4031, name: "Rotating Arm Wrapping" },
+              { id: 4032, name: "Rotating Table Wrapping" },
+              { id: 4033, name: "Wrapping Robots" },
+              { id: 4034, name: "Heat Shrink Ovens" },
+            ],
+          },
+          {
+            id: 404,
+            name: "Industrial Vacuum Cleaner",
+            subCategories: [
+              { id: 4041, name: "Dust and Chip Aspirators" },
+              { id: 4042, name: "Oil Aspirators" },
+            ],
+          },
+          {
+            id: 406,
+            name: "Safety Solutions",
+            subCategories: [
+              { id: 4061, name: "Safety Barriers" },
+              { id: 4062, name: "Visual Management Tools" },
+              { id: 4063, name: "Safety Lighting" },
+            ],
+          },
           { id: 407, name: "Electric Cutters" },
           { id: 408, name: "Industrial Doors" },
           { id: 409, name: "Industrial Flooring Solutions" },
           { id: 410, name: "Industrial Springs" },
           { id: 411, name: "Turbine Roof Ventilators" },
           { id: 412, name: "Industrial Filtration" },
-          { id: 413, name: "Nitrogen & Oxygen Generators" },
+          {
+            id: 413,
+            name: "Nitrogen & Oxygen Generators",
+            subCategories: [
+              { id: 4131, name: "Oxygen Generators" },
+              { id: 4132, name: "Nitrogen Generators" },
+            ],
+          },
         ],
       };
 
