405f6dd53a962072e447eff86b612941467b6c7d
Mishab Ameer <mishab@dexlock.com>
2025-01-08 18:07:46 +0300

    Added image preview modal

--- app/product/[productDetails].tsx
+++ app/product/[productDetails].tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Dimensions,
   FlatList,
@@ -6,6 +6,7 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 import { useLocalSearchParams } from "expo-router";
@@ -13,28 +14,47 @@ import VideoSection from "../../src/components/videoSection";
 import { PRIMARY_THEME } from "../../src/constants";
 import GetProductData from "../../src/GetProductData";
 import CustomHeader from "@/src/components/customHeader";
+import { ImageSourcePropType } from "react-native";
+import { Modal } from "react-native";
 
 interface ImageData {
   id: number;
-  path: string;
+  path: ImageSourcePropType;
 }
+const { width: screenWidth } = Dimensions.get("window");
 
 const ProductDetails: React.FC = () => {
   const { productDetails } = useLocalSearchParams<{ productDetails: string }>();
   const productData = GetProductData(productDetails);
   const numColumns = 3;
   const itemWidth = Dimensions.get("window").width / numColumns - 40;
+  const [isPreviewVisible, setIsPreviewVisible] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const renderItem = ({ item }: { item: ImageData }) => {
-    return (
+  const handleImagePress = (index: number) => {
+    setSelectedIndex(index);
+    setIsPreviewVisible(true);
+  };
+
+  const renderItem = ({ item, index }: { item: ImageData; index: number }) => (
+    <TouchableOpacity onPress={() => handleImagePress(index)}>
       <View style={styles.card}>
         <Image
           source={item.path}
           style={[styles.image, { width: itemWidth, height: itemWidth }]}
         />
       </View>
-    );
-  };
+    </TouchableOpacity>
+  );
+  const renderPreviewItem = ({ item }: { item: ImageData }) => (
+    <View style={styles.fullscreenImageContainer}>
+      <Image
+        source={item.path}
+        style={styles.fullscreenImage}
+        resizeMode="contain"
+      />
+    </View>
+  );
 
   return (
     <ScrollView style={styles.container}>
@@ -53,6 +73,31 @@ const ProductDetails: React.FC = () => {
           <VideoSection videoData={productData?.videos} />
         </View>
       )}
+      <Modal
+        visible={isPreviewVisible}
+        transparent={true}
+        onRequestClose={() => setIsPreviewVisible(false)}
+      >
+        <FlatList
+          data={productData?.images}
+          renderItem={renderPreviewItem}
+          keyExtractor={(item) => item.id.toString()}
+          horizontal
+          pagingEnabled
+          initialScrollIndex={selectedIndex}
+          getItemLayout={(data, index) => ({
+            length: screenWidth,
+            offset: screenWidth * index,
+            index,
+          })}
+          onMomentumScrollEnd={(event) => {
+            const index = Math.round(
+              event.nativeEvent.contentOffset.x / screenWidth
+            );
+            setSelectedIndex(index);
+          }}
+        />
+      </Modal>
     </ScrollView>
   );
 };
@@ -74,9 +119,22 @@ const styles = StyleSheet.create({
   card: {
     margin: 10,
     alignItems: "center",
+    backgroundColor: "#FFF",
+    borderRadius: 10,
   },
   video: {
     width: "100%",
     height: 300,
+  },
+  fullscreenImageContainer: {
+    width: screenWidth,
+    height: "90%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  fullscreenImage: {
+    width: "100%",
+    height: "100%",
   },
 });
